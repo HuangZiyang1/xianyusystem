c420c27e9bb34685ce4154ee41c8cf7a5c504cac
newpc <2258532120@qq.com>
2023-11-29 21:48:50 +0800

    feat：闲置功能、雷同书籍，

---
+++ src/api/idle.js
@@ -0,0 +1,48 @@
+import request from '@/utils/request'
+
+export function fetchList() {
+  return request({
+    url: '/admin/freeBusiness/selectFreeBusinessesAll?currentPage=1&&pageSize=5',
+    method: 'get',
+  })
+}
+
+export function searchBook(searchBlank) {
+  return request({
+    url: '/admin/freeBusiness/searchFreeBusinessesAll',
+    method: 'get',
+    params:{
+      searchBlank
+    }
+  })
+}
+
+export function deleteByBookSuitId(businessId) {
+  return request({
+    url: '/admin/freeBusiness/deleteFreeBusiness',
+    method: 'post',
+    data: {
+      businessId
+    }
+  })
+}
+
+export function topBook(businessId) {
+  return request({
+    url: '/admin/freeBusiness/topFreeBusinesses',
+    method: 'post',
+    data: {
+      businessId
+    }
+  })
+}
+
+export function cancelTopBook(businessId) {
+  return request({
+    url: '/admin/freeBusiness/cancelTopFreeBusinesses',
+    method: 'post',
+    data: {
+      businessId
+    }
+  })
+}
--- src/views/worktable/search-idle.vue
+++ src/views/worktable/search-idle.vue
@@ -1,11 +1,8 @@
 <template>
   <div class="app-container">
     <div class="filter-container">
-      <el-input v-model="listQuery.title" placeholder="商品名称" style="width: 150px;" class="filter-item" @keyup.enter.native="handleFilter" />
-      <el-select v-model="listQuery.importance" placeholder="商品类型" clearable style="width: 110px" class="filter-item">
-        <el-option v-for="item in importanceOptions" :key="item" :label="item" :value="item" />
-      </el-select>
-      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="handleFilter">
+      <el-input v-model="listQuery.title" placeholder="搜索闲置标题、闲置简介" style="width: 320px;" class="filter-item" @keyup.enter.native="searchBlank" minlength="1" maxlength="32" show-word-limit/>
+      <el-button v-waves class="filter-item" type="primary" icon="el-icon-search" @click="searchBlank">
         搜索
       </el-button>
     </div>
@@ -18,63 +15,58 @@
       fit
       highlight-current-row
       style="width: 100%;"
-      @sort-change="sortChange"
     >
-      <el-table-column label="商品id" prop="id" sortable="custom" align="center" width="120" :class-name="getSortClass('id')">
+      <el-table-column label="闲置id" prop="id" sortable="custom" align="center" width="120">
         <template slot-scope="{row}">
-          <span>{{ row.id }}</span>
+          <span>{{ row.businessId }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="发布时间" width="150px" align="center">
+      <el-table-column label="发布时间" width="200" align="center">
         <template slot-scope="{row}">
-          <span>{{ row.timestamp | parseTime('{y}-{m}-{d} {h}:{i}') }}</span>
+          <span>{{ row.createTime }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="商品名称" min-width="150px" style="display:flex;align-items: center;">
+      <el-table-column label="闲置图片" min-width="120" width="150" style="display:flex;align-items: center;">
         <template slot-scope="{row}">
-          <img src="@/assets/images/service.png" alt="" style="height:60px;width:auto">
-          <span class="link-type" @click="handleUpdate(row)">{{ row.title }}</span>
+          <img :src="row.pictureList ? row.pictureList[0].url : ''" alt="" style="height:60px;width:auto">
         </template>
       </el-table-column>
-      <el-table-column label="商品类型" min-width="50px" style="display:flex;align-items: center;">
+      <el-table-column label="闲置名称" width="500" align="center">
         <template slot-scope="{row}">
-          <el-tag>{{ row.type | typeFilter }}</el-tag>
+          <el-row v-for="item in row.books" :key="item.bookId">
+            <span style="margin-right:6px;">闲置名称：{{ item.businessName }}</span>
+            <!-- <span>书籍ISBN号：{{ item.isbn }}</span> -->
+          </el-row>
         </template>
       </el-table-column>
-      <el-table-column label="商品上传者" width="110px" align="center">
+      <el-table-column label="闲置简介" width="110" align="center">
         <template slot-scope="{row}">
-          <span>{{ row.author }}</span>
+          <span>{{ row.info }}</span>
         </template>
       </el-table-column>
-
-
-
-      <!-- <el-table-column v-if="showReviewer" label="Reviewer" width="110px" align="center">
+      <el-table-column label="闲置上传者" width="110" align="center">
         <template slot-scope="{row}">
-          <span style="color:red;">{{ row.reviewer }}</span>
-        </template>
-      </el-table-column> -->
-
-      <el-table-column label="浏览数" align="center" width="95">
-        <template slot-scope="{row}">
-          <span v-if="row.pageviews" class="link-type" @click="handleFetchPv(row.pageviews)">{{ row.pageviews }}</span>
-          <span v-else>0</span>
+          <span>{{ row.userId }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="评论数" align="center" width="95">
+      <el-table-column label="闲置价格区间" width="110" align="center">
         <template slot-scope="{row}">
-          <span v-if="row.pageviews" class="link-type" @click="handleFetchPv(row.pageviews)">{{ row.pageviews }}</span>
-          <span v-else>0</span>
+          <span>{{ row.lowPrice + '~' + row.highPrice }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="收藏数" align="center" width="95">
+      <el-table-column label="浏览数" align="center" width="65">
         <template slot-scope="{row}">
-          <span v-if="row.pageviews" class="link-type" @click="handleFetchPv(row.pageviews)">{{ row.pageviews }}</span>
-          <span v-else>0</span>
+          <span class="link-type">{{ row.pageView }}</span>
         </template>
       </el-table-column>
-      <el-table-column label="操作" align="center" width="280" class-name="small-padding fixed-width">
+      <el-table-column label="操作" align="center" width="235" class-name="small-padding fixed-width">
         <template slot-scope="{row,$index}">
+          <el-button v-if="row.isOnTop!=1" size="mini" type="primary" @click="handleTop(row,$index)">
+            置顶
+          </el-button>
+          <el-button v-if="row.isOnTop!=0" size="mini" type="info" @click="cancelTopBook(row,$index)">
+            取消置顶
+          </el-button>
           <el-button v-if="row.status!='deleted'" size="mini" type="danger" @click="handleDelete(row,$index)">
             删除
           </el-button>
@@ -83,96 +75,25 @@
     </el-table>
 
     <pagination v-show="total>0" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.limit" @pagination="getList" />
-
-    <el-dialog :title="textMap[dialogStatus]" :visible.sync="dialogFormVisible">
-      <el-form ref="dataForm" :rules="rules" :model="temp" label-position="left" label-width="70px" style="width: 400px; margin-left:50px;">
-        <el-form-item label="Type" prop="type">
-          <el-select v-model="temp.type" class="filter-item" placeholder="Please select">
-            <el-option v-for="item in calendarTypeOptions" :key="item.key" :label="item.display_name" :value="item.key" />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="Date" prop="timestamp">
-          <el-date-picker v-model="temp.timestamp" type="datetime" placeholder="Please pick a date" />
-        </el-form-item>
-        <el-form-item label="Title" prop="title">
-          <el-input v-model="temp.title" />
-        </el-form-item>
-        <el-form-item label="Status">
-          <el-select v-model="temp.status" class="filter-item" placeholder="Please select">
-            <el-option v-for="item in statusOptions" :key="item" :label="item" :value="item" />
-          </el-select>
-        </el-form-item>
-        <el-form-item label="Imp">
-          <el-rate v-model="temp.importance" :colors="['#99A9BF', '#F7BA2A', '#FF9900']" :max="3" style="margin-top:8px;" />
-        </el-form-item>
-        <el-form-item label="Remark">
-          <el-input v-model="temp.remark" :autosize="{ minRows: 2, maxRows: 4}" type="textarea" placeholder="Please input" />
-        </el-form-item>
-      </el-form>
-      <div slot="footer" class="dialog-footer">
-        <el-button @click="dialogFormVisible = false">
-          Cancel
-        </el-button>
-        <el-button type="primary" @click="dialogStatus==='create'?createData():updateData()">
-          Confirm
-        </el-button>
-      </div>
-    </el-dialog>
-
-    <el-dialog :visible.sync="dialogPvVisible" title="Reading statistics">
-      <el-table :data="pvData" border fit highlight-current-row style="width: 100%">
-        <el-table-column prop="key" label="Channel" />
-        <el-table-column prop="pv" label="Pv" />
-      </el-table>
-      <span slot="footer" class="dialog-footer">
-        <el-button type="primary" @click="dialogPvVisible = false">Confirm</el-button>
-      </span>
-    </el-dialog>
   </div>
 </template>
 
 <script>
-import { fetchList, fetchPv, createArticle, updateArticle } from '@/api/article'
+import { deleteByBookSuitId, fetchList,searchBook,topBook,cancelTopBook } from '@/api/idle'
 import waves from '@/directive/waves' // waves directive
-import { parseTime } from '@/utils/index.js'
 import Pagination from '@/components/Pagination' // secondary package based on el-pagination
 
-const calendarTypeOptions = [
-  { key: 'CN', display_name: 'China' },
-  { key: 'US', display_name: 'USA' },
-  { key: 'JP', display_name: 'Japan' },
-  { key: 'EU', display_name: 'Eurozone' }
-]
-
-// arr to obj, such as { CN : "China", US : "USA" }
-const calendarTypeKeyValue = calendarTypeOptions.reduce((acc, cur) => {
-  acc[cur.key] = cur.display_name
-  return acc
-}, {})
-
 export default {
-  name: 'WorkTable',
+  name: 'SearchIdle',
   components: { Pagination },
   directives: { waves },
-  filters: {
-    statusFilter(status) {
-      const statusMap = {
-        published: 'success',
-        draft: 'info',
-        deleted: 'danger'
-      }
-      return statusMap[status]
-    },
-    typeFilter(type) {
-      return calendarTypeKeyValue[type]
-    }
-  },
   data() {
     return {
       tableKey: 0,
       list: null,
       total: 0,
       listLoading: true,
+      allList: null,
       listQuery: {
         page: 1,
         limit: 20,
@@ -181,47 +102,8 @@ export default {
         type: undefined,
         sort: '+id'
       },
-      importanceOptions: [
-        {
-          typeId:1,
-          typeName: '书籍'
-        },
-        {
-          typeId:2,
-          typeName: '闲置'
-        },
-        {
-          typeId:3,
-          typeName: '服务'
-        }
-      ],
-      calendarTypeOptions,
-      sortOptions: [{ label: '正序', key: '+id' }, { label: '倒序', key: '-id' }],
-      statusOptions: ['published', 'draft', 'deleted'],
-      showReviewer: false,
-      temp: {
-        id: undefined,
-        importance: 1,
-        remark: '',
-        timestamp: new Date(),
-        title: '',
-        type: '',
-        status: 'published'
-      },
-      dialogFormVisible: false,
-      dialogStatus: '',
-      textMap: {
-        update: 'Edit',
-        create: 'Create'
-      },
-      dialogPvVisible: false,
-      pvData: [],
-      rules: {
-        type: [{ required: true, message: 'type is required', trigger: 'change' }],
-        timestamp: [{ type: 'date', required: true, message: 'timestamp is required', trigger: 'change' }],
-        title: [{ required: true, message: 'title is required', trigger: 'blur' }]
-      },
-      downloadLoading: false
+      downloadLoading: false,
+      picList:[]
     }
   },
   created() {
@@ -230,19 +112,13 @@ export default {
   methods: {
     getList() {
       this.listLoading = true
-      fetchList(this.listQuery).then(response => {
-        this.list = response.data.items
-        this.total = response.data.total
-
-        // Just to simulate the time of the request
+      fetchList().then(response => {
+        this.list = response.data
+        this.allList = this.list
         setTimeout(() => {
           this.listLoading = false
         }, 1.5 * 1000)
       })
-    },
-    handleFilter() {
-      this.listQuery.page = 1
-      this.getList()
     },
     handleModifyStatus(row, status) {
       this.$message({
@@ -251,127 +127,72 @@ export default {
       })
       row.status = status
     },
-    sortChange(data) {
-      const { prop, order } = data
-      if (prop === 'id') {
-        this.sortByID(order)
-      }
-    },
-    sortByID(order) {
-      if (order === 'ascending') {
-        this.listQuery.sort = '+id'
-      } else {
-        this.listQuery.sort = '-id'
-      }
-      this.handleFilter()
-    },
-    resetTemp() {
-      this.temp = {
-        id: undefined,
-        importance: 1,
-        remark: '',
-        timestamp: new Date(),
-        title: '',
-        status: 'published',
-        type: ''
-      }
-    },
-    handleCreate() {
-      this.resetTemp()
-      this.dialogStatus = 'create'
-      this.dialogFormVisible = true
-      this.$nextTick(() => {
-        this.$refs['dataForm'].clearValidate()
-      })
-    },
-    createData() {
-      this.$refs['dataForm'].validate((valid) => {
-        if (valid) {
-          this.temp.id = parseInt(Math.random() * 100) + 1024 // mock a id
-          this.temp.author = 'vue-element-admin'
-          createArticle(this.temp).then(() => {
-            this.list.unshift(this.temp)
-            this.dialogFormVisible = false
-            this.$notify({
-              title: 'Success',
-              message: 'Created Successfully',
-              type: 'success',
-              duration: 2000
-            })
-          })
-        }
-      })
-    },
-    handleUpdate(row) {
-      this.temp = Object.assign({}, row) // copy obj
-      this.temp.timestamp = new Date(this.temp.timestamp)
-      this.dialogStatus = 'update'
-      this.dialogFormVisible = true
-      this.$nextTick(() => {
-        this.$refs['dataForm'].clearValidate()
-      })
-    },
-    updateData() {
-      this.$refs['dataForm'].validate((valid) => {
-        if (valid) {
-          const tempData = Object.assign({}, this.temp)
-          tempData.timestamp = +new Date(tempData.timestamp) // change Thu Nov 30 2017 16:41:05 GMT+0800 (CST) to 1512031311464
-          updateArticle(tempData).then(() => {
-            const index = this.list.findIndex(v => v.id === this.temp.id)
-            this.list.splice(index, 1, this.temp)
-            this.dialogFormVisible = false
-            this.$notify({
-              title: 'Success',
-              message: 'Update Successfully',
-              type: 'success',
-              duration: 2000
-            })
-          })
-        }
-      })
-    },
+
     handleDelete(row, index) {
-      this.$notify({
-        title: 'Success',
-        message: 'Delete Successfully',
+      console.log(row.suitId);
+      deleteByBookSuitId(row.businessId).then(response => {
+        console.log(response)
+        this.$notify({
+        title: '成功',
+        message: response.msg,
         type: 'success',
         duration: 2000
       })
+      // 确定OK再删除
       this.list.splice(index, 1)
-    },
-    handleFetchPv(pv) {
-      fetchPv(pv).then(response => {
-        this.pvData = response.data.pvData
-        this.dialogPvVisible = true
+      }).catch(err => {
+        console.log(err);
       })
+
+
     },
-    // handleDownload() {
-    //   this.downloadLoading = true
-    //   import('@/vendor/Export2Excel').then(excel => {
-    //     const tHeader = ['timestamp', 'title', 'type', 'importance', 'status']
-    //     const filterVal = ['timestamp', 'title', 'type', 'importance', 'status']
-    //     const data = this.formatJson(filterVal)
-    //     excel.export_json_to_excel({
-    //       header: tHeader,
-    //       data,
-    //       filename: 'table-list'
-    //     })
-    //     this.downloadLoading = false
-    //   })
-    // },
-    formatJson(filterVal) {
-      return this.list.map(v => filterVal.map(j => {
-        if (j === 'timestamp') {
-          return parseTime(v[j])
-        } else {
-          return v[j]
-        }
-      }))
+    searchBlank() {
+      let title = this.listQuery.title
+      searchBook(title).then(response => {
+        console.log(response)
+        this.list = response.data
+      }).catch(err => {
+        this.list = this.allList
+        console.log(err);
+      })
+
     },
-    getSortClass: function(key) {
-      const sort = this.listQuery.sort
-      return sort === `+${key}` ? 'ascending' : 'descending'
-    }
-  }
+
+    handleTop(row, index) {
+      console.log(row.businessId);
+      topBook(row.businessId).then(response => {
+        console.log(response)
+        this.$notify({
+        title: '成功',
+        message: response.msg,
+        type: 'success',
+        duration: 2000
+      })
+      row.isOnTop = 1
+      }).catch(err => {
+        console.log(err);
+      })
+
+
+    },
+    cancelTopBook(row, index) {
+      console.log(row.businessId);
+      cancelTopBook(row.businessId).then(response => {
+        console.log(response)
+        this.$notify({
+        title: '成功',
+        message: response.msg,
+        type: 'success',
+        duration: 2000
+      })
+      row.isOnTop = 0
+      }).catch(err => {
+        console.log(err);
+      })
+
+
+    },
+
+  },
 }
 </script>
